9295b84d9ba9ac5ae5394a02c0378eca6e56331c
Harm Brouwer <me@hbrouwer.eu>
2012-12-10 16:24:11 +0100

    added general description

--- src/coals.c
+++ src/coals.c
@@ -38,9 +38,24 @@
 
 /*
  * This implements the Correlated Occurrence Analogue to Lexical Semantics
- * (COALS; Rohde, Gonnerman, & Plaut, 2005).
+ * (COALS; Rohde, Gonnerman, & Plaut, 2005). COALS represents word meaning
+ * by means of high-dimensional vectors derived from word co-occurrence
+ * patterns in large corporora. The construction of these vectors starts by
+ * compiling a co-occurrence table using a ramped (or alternatively, flat)
+ * n-word window. Rohde et al. use a 4-word, ramped window:
  *
- * (description goes here)
+ *     1 2 3 4 [0] 4 3 2 1
+ *
+ * Next, all but the m (14.000 in case of Rohde et al.) columns, reflecting
+ * the most frequent words, are discarded. Co-occurrence counts are then
+ * converted to word-pair correlations; negative correlations set to zero,
+ * and positive correlations are replaced by their square root in order to
+ * magnify them. The rows of the co-occurrence table then represent COALS
+ * vectors for their respective words. Optionally, Singular Value
+ * Decomposition (SVD) can be used to reduce the dimensionality of these
+ * vectors, and these reduced vectors can, in turn, be converted to binary
+ * vectors, by setting negative components to zero, and positive components
+ * to one.
  *
  * References
  *
